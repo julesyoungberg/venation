9995866d7e1279ac9f101798ba634034c394ce0e
Jules Youngberg <julesdayoungberg@gmail.com>
2021-06-05 22:30:45 -0700

    old-school open gl style

--- src/app.cpp
+++ src/app.cpp
@@ -1,56 +1,45 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
+#include <glm/gtc/random.hpp>
 
 #ifdef __APPLE__
 #include <OpenGL/gl3.h>
 #endif
 
 #include "./app.hpp"
-#include "./shader.hpp"
 
-App::~App() {
-    glDeleteShader(attractors_vert_shader_);
-    glDeleteShader(attractors_frag_shader_);
+float rnd() {
+    return static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 }
 
 void App::setup() {
     // generate attractors
-    attractors_.push_back(glm::vec3(0.0f, 0.0f, 0.0f));
+    constexpr int num_attractors = 1000;
+    for (int i = 0; i < num_attractors; ++i) {
+        attractors_.push_back(
+            glm::vec2(
+                rnd() * 2.0f - 1.0f,
+                rnd() * 2.0f - 1.0f
+            )
+        );
+    }
+}
 
-    // setup gpu attractors buffer
-    glGenBuffers(1, &vbo_);
-    glGenVertexArrays(1, &vao_);
-    glBindVertexArray(vao_);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo_);
-    glBufferData(GL_ARRAY_BUFFER, attractors_.size() * 3 * sizeof(float), 
-            attractors_.data(), GL_DYNAMIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // compile attractors shaders
-    const char *vert_source = "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "void main(){\n"
-        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-        "}\0";
-    attractors_vert_shader_ = shader::compile_string(&vert_source, GL_VERTEX_SHADER);
-    attractors_frag_shader_ = shader::compile_file("basic.frag", GL_FRAGMENT_SHADER);
-
-    // create attractors program
-    attractors_program_ = shader::create_program(
-        attractors_vert_shader_, 
-        attractors_frag_shader_
-    );
+void App::draw_attractors() {
+    glBegin(GL_POINTS); 
+        glPointSize(10.0f);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        for (glm::vec2 v : attractors_) {
+            glVertex2f(v.x, v.y);
+        }
+    glEnd();
 }
 
 void App::draw() {
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glPointSize(10.0f);
-    glUseProgram(attractors_program_);
-    glBindVertexArray(vao_);
-    glDrawArrays(GL_POINTS, 0, 3);
+    draw_attractors();
 
     glFlush();
 }
--- src/app.hpp
+++ src/app.hpp
@@ -4,23 +4,18 @@
 
 #include "glm/glm.hpp"
 
-#include "./shader.hpp"
-
 class App {
     public:
         App() = default;
-        ~App();
+        ~App() = default;
 
         void setup();
         void draw();
 
     private:
-        std::vector<glm::vec3> attractors_;
-        unsigned int vbo_;
-        unsigned int vao_;
-        unsigned int attractors_program_;
+        void draw_attractors();
 
-        unsigned int attractors_vert_shader_;
-        unsigned int attractors_frag_shader_;
+        std::vector<glm::vec2> attractors_;
+
 };
 
